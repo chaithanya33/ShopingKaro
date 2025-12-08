03d758f3f2c5861cfad464857e6a5706ae9e5d20
tariq <tariq@meetdeal.fr>
2025-12-08 16:28:21 +0100

    refactor: cleaning

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -10,22 +10,6 @@ variable "project_name" {
   default     = "shopingkaro"
 }
 
-variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
-  type        = string
-  default     = "10.0.0.0/16"
-}
-
-variable "public_subnet_cidrs" {
-  type    = list(string)
-  default = ["10.0.1.0/24", "10.0.2.0/24"]
-}
-
-variable "private_subnet_cidrs" {
-  type    = list(string)
-  default = ["10.0.101.0/24", "10.0.102.0/24"]
-}
-
 variable "ecs_desired_count" {
   type    = number
   default = 1
