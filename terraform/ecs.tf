e6baa1b82dc7f746dc0506dd1f42959e5c460e7b
tariq <tariq@meetdeal.fr>
2025-12-09 10:27:42 +0100

    fix: terraform

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -16,6 +16,8 @@ resource "aws_lb_target_group" "tg" {
   port     = 3000
   protocol = "HTTP"
   vpc_id   = var.vpc_id
+  target_type = "ip"
+
   health_check {
     path                = "/"
     matcher             = "200-399"
